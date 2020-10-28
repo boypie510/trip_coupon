102ddaf2669a826d05710eb60c36f213a9df06e7
Leo Li <boypie510@gmail.com>
2020-10-28 15:41:43 +0800

    correct devise and set /coupon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    # redirect_to discover_path if user_signed_in?
+    redirect_to coupon_path if user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :models
+  devise_for :users
+  # devise_for :models
   root to: "home#index"
+  get '/coupon', to: "home#coupon"
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions',
+  #   registrations: 'users/registrations'
+  #     }
+      
 end
